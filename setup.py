1de220d12983ce964711c30999a2bb6136ab84bd
Marcus Martins <marcus.martins@sisqualis.com.br>
2015-08-20 16:42:30 -0300

    Adds requirements, coveragerc and setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-pagseguro',
+    version='0.1.0',
+    description='PagSeguro lib that suports manual subscriptions',
+    url='',
+    author='Mbodock',
+    author_email='mbodock@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Developer Status :: 4 - Beta',
+    ],
+    keywords='pagseguro subscription',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'arrow',
+        'beautifulsoup4',
+        'requests',
+        'Unidecode',
+    ],
+)
